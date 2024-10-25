f9994822e94e736fb7143cc0f3ffa6ad70251254
fountain789 <efountain6873@gmail.com>
2024-10-25 18:05:37 -0400

    Update App.js
    what am i doing wrong

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   }, []);
 
   return (
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -22,7 +23,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div></>
+    </div>
   );
 }
 
