909f784224d8a3414d3c99651448003f84f46bd4
fountain789 <efountain6873@gmail.com>
2024-10-25 18:03:39 -0400

    Update App.js
    trying again!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "Smart Water Heater Controller";
+  }, []);
+
   return (
-    <><title>Smart Water Heater Controller</title><div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
